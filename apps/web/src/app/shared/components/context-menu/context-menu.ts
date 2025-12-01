ffb79f841a232e7abeb6a1f562c9e0bdb4fadcdb
Roberth <roberthchirinos87@gmail.com>
2025-12-01 01:35:56 +0100

    Hide context-menu on scroll

--- apps/web/src/app/shared/components/context-menu/context-menu.ts
+++ apps/web/src/app/shared/components/context-menu/context-menu.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, OnDestroy, input, viewChild } from '@angular/core';
 import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
@@ -11,8 +11,25 @@ import { NgClass } from '@angular/common';
   templateUrl: './context-menu.html',
   styleUrl: './context-menu.scss',
 })
-export class ContextMenu {
+export class ContextMenu implements OnDestroy {
   menuItems = input.required<MenuItem[]>();
   alignment = input<MenuAlignment>('end');
   protected readonly faEllipsisVertical = faEllipsisVertical;
+  menuTrigger = viewChild(CdkMenuTrigger);
+
+  constructor() {
+    document.addEventListener('scroll', this.handleScroll, true);
+  }
+
+/**
+ * Removes the event listener for the scroll event and closes the menu when the component is destroyed.
+ */
+  ngOnDestroy(): void {
+    document.removeEventListener('scroll', this.handleScroll, true);
+    this.menuTrigger()?.close();
+  }
+
+  private handleScroll = () => {
+    this.menuTrigger()?.close();
+  };
 }
